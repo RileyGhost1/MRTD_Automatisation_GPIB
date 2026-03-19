b30b3585ed744c70c1bbe92d8c01a04b118730a3
Kaan061 <merictr61@gmail.com>
2026-03-19 15:36:35 +0100

    3 butons on final UI has been added, some probleme with boxes should be fixed in next commit

--- src/hmi/hmi_ui.c
+++ src/hmi/hmi_ui.c
@@ -1,41 +1,97 @@
 #include <gtk/gtk.h>
 
+/*
+ * Callback function for the increment temperature button
+ */
 static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
+on_inc_temp_clicked(GtkWidget *widget, gpointer user_data)
 {
-  g_print ("Hello World\n");
+    // TODO: plus tard -> appeler gpib_set_temp(current + step);
+    g_print("Augmenter température\n");
 }
 
 static void
-activate (GtkApplication *app,
-          gpointer        user_data)
+on_dec_temp_clicked(GtkWidget *widget, gpointer user_data)
 {
-  GtkWidget *window;
-  GtkWidget *button;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Hello");
-  #ifdef __arm__
-        // Si on est sur Raspberry Pi (Architecture ARM)
-        gtk_window_fullscreen(GTK_WINDOW(window));
-        // On peut aussi cacher le curseur si c'est un écran tactile
-        gtk_widget_set_cursor_from_name(window, "none"); 
-    #else
-        // Si on est sur PC (Windows/Autre)
-        gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
-    #endif
-
-  button = gtk_button_new_with_label ("Hello World");
-  gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
-  gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  gtk_window_set_child (GTK_WINDOW (window), button);
-
-  gtk_window_present (GTK_WINDOW (window));
+    // TODO: plus tard -> appeler gpib_set_temp(current - step);
+    g_print("Diminuer température\n");
 }
 
-int ui_run(int    argc, char **argv)
+static void
+on_target_temp_clicked(GtkWidget *widget, gpointer user_data)
+{
+    // TODO: plus tard -> appeler gpib_next_target(target);
+    g_print("Prochaine cible demandée...\n");
+}
+
+static void
+activate (GtkApplication *app, gpointer user_data)
+{
+    GtkWidget *window;
+    GtkWidget *box_temp;
+    GtkWidget *box_target;
+    GtkWidget *btn_inc;
+    GtkWidget *btn_dec;
+    GtkWidget *btn_target;
+
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "MRTD Control");
+
+#ifdef __arm__
+    gtk_window_fullscreen(GTK_WINDOW(window));
+#else
+    gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
+#endif
+
+    /* Appliquer le style CSS */
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(provider, "../src/hmi/gtk.css");
+    gtk_style_context_add_provider_for_display(
+        gdk_display_get_default(),
+        GTK_STYLE_PROVIDER(provider),
+        GTK_STYLE_PROVIDER_PRIORITY_USER);
+    g_object_unref(provider);
+
+    /* box_temp */
+    box_temp = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
+    gtk_widget_set_halign(box_temp, GTK_ALIGN_START);
+    gtk_widget_set_margin_start(box_temp, 40);
+    gtk_widget_set_valign(box_temp, GTK_ALIGN_CENTER);
+
+    /* box_target */
+    box_target = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
+    gtk_widget_set_halign(box_target, GTK_ALIGN_END);
+    gtk_widget_set_margin_start(box_target, 40);
+    gtk_widget_set_valign(box_target, GTK_ALIGN_CENTER);
+
+    /* bouton + */
+    btn_inc = gtk_button_new_with_label(" + Temp ");
+    gtk_widget_add_css_class(btn_inc, "big");
+    g_signal_connect(btn_inc, "clicked",
+                     G_CALLBACK(on_inc_temp_clicked), NULL);
+
+    /* bouton - */
+    btn_dec = gtk_button_new_with_label(" - Temp ");
+    gtk_widget_add_css_class(btn_dec, "big");
+    g_signal_connect(btn_dec, "clicked",
+                     G_CALLBACK(on_dec_temp_clicked), NULL);
+    
+    /* bouton cible */
+    btn_target = gtk_button_new_with_label(" Next Target ");
+    gtk_widget_add_css_class(btn_target, "big");
+    g_signal_connect(btn_target, "clicked",
+                     G_CALLBACK(on_target_temp_clicked), NULL);
+
+    gtk_box_append(GTK_BOX(box_temp), btn_inc);
+    gtk_box_append(GTK_BOX(box_temp), btn_dec);
+    gtk_box_append(GTK_BOX(box_target), btn_target);
+
+    gtk_window_set_child(GTK_WINDOW(window), box_temp);
+    gtk_window_set_child(GTK_WINDOW(window), box_target);
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int ui_run(int argc, char **argv)
 {
   GtkApplication *app;
   int status;
